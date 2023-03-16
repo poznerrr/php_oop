38408b03e13eea6a781270a7fd8db2ef7248d50f
poznerrr <pozner@gmail.com>
2023-03-16 12:13:28 +0300

    feat: add attributes

---
+++ 01/classes/Car.php
@@ -0,0 +1,9 @@
+<?php
+class Car
+{
+  public $color;
+  public $wheels;
+  public $speed;
+
+}
+?>
--- 01/index.php
+++ 01/index.php
@@ -1,8 +1,28 @@
 <?php
 require_once 'classes/FirstClass.php';
+require_once 'classes/Car.php';
+
 
 $o1 = new FirstClass;
 $o2 = new FirstClass;
-var_dump($o1);
-var_dump($o2);
+
+$car1 = new Car;
+$car2 = new Car;
+
+$car1->color = 'black';
+$car1->wheels = 4;
+$car1->speed = 120;
+
+debug($car1);
+debug($car2);
+
+echo "<h3> Об авто </h3>
+Цвет: {$car1->color} <br>
+Колёс: {$car1->wheels} <br>
+Скорость: {$car1->speed}  км/ч <br>
+";
+
+function debug($data) {
+  echo '<pre>'.print_r($data, 1).'</pre>';
+}
 ?>
